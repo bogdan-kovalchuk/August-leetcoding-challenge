d621721f6313d26059fe6df6d086d9b812fc6d7f
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-08-14 10:03:52 +0300

    day 14 commit

---
+++ day_14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        return 0;
+    }
+};
+
+int main() {
+    string s = "abccccdd";
+    Solution solution;
+    std::cout << solution.longestPalindrome(s) << std::endl;
+    return 0;
+}
+
