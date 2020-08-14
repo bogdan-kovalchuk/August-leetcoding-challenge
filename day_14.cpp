b5eca7669a0ae909966cfda7a04a776140543ebd
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-08-14 10:33:28 +0300

    day 14 commit

--- day_14.cpp
+++ day_14.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 using std::string;
+using std::unordered_map;
 
 class Solution {
 public:
     int longestPalindrome(string s) {
-        return 0;
+        if (s.empty()) return 0;
+        unordered_map<char, int> m;
+        int p_len = 0, odd_num = 0;
+        for (const auto &ch : s) m[ch]++;
+
+        for (const auto &item : m) {
+            if (item.second % 2 == 0) {
+                p_len += item.second;
+            } else if (item.second > 2 && item.second % 2 != 0){
+                p_len += item.second - 1;
+                ++odd_num;
+            } else {
+                ++odd_num;
+            }
+        }
+        return odd_num > 0 ? p_len + 1 : p_len;
     }
 };
 
