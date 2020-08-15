bf336e9b0036a81cb4363827be423d4c89551ac0
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-08-15 13:07:12 +0300

    day 15 commit

--- day_15.cpp
+++ day_15.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>> &intervals) {
-        return 0;
+        if (intervals.size() == 0) return 0;
+        int res = 0;
+        std::sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int> &b){
+            return a[1] < b[1];
+        });
+        int prev_end = intervals[0][1];
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i][0] < prev_end) {
+                ++res;
+            }
+            else {
+                prev_end = intervals[i][1];
+            }
+        }
+        return res;
     }
 };
-
 int main() {
     vector<vector<int>> intervals = {{1, 2},
                                      {2, 3},
