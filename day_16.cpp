f0e8da0018246687471fd3196b41fa7cad8c62cb
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-08-16 18:25:30 +0300

    day 16 commit

--- day_16.cpp
+++ day_16.cpp
@@ -5,8 +5,17 @@ using std::vector;
 
 class Solution {
 public:
-    int maxProfit(vector<int> &prices) {
-        return 0;
+    int maxProfit(vector<int> prices) {
+        if (prices.empty()) return 0;
+        vector<vector<int>> dp(3, vector<int>(prices.size(), 0));
+        for (int k = 1; k <= 2; k++) {
+            int min = prices[0];
+            for (int i = 1; i < prices.size(); i++) {
+                min = std::min(min, prices[i] - dp[k - 1][i - 1]);
+                dp[k][i] = std::max(dp[k][i - 1], prices[i] - min);
+            }
+        }
+        return dp[2][prices.size() - 1];
     }
 };
 
