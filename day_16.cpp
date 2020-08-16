5e7e485098508e3f89aa937cdf0b773b474c15b0
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-08-16 11:35:34 +0300

    day 16 commit

---
+++ day_16.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        return 0;
+    }
+};
+
+int main() {
+    vector<int> prices = {3, 3, 5, 0, 0, 3, 1, 4};
+    Solution solution;
+    std::cout << solution.maxProfit(prices) << std::endl;
+    return 0;
+}
+
