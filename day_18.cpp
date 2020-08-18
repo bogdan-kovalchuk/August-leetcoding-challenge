0be34ad72d456fffb29e5a641a66102cadb4043b
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-08-18 14:35:37 +0300

    day 18 commit

--- day_18.cpp
+++ day_18.cpp
@@ -6,7 +6,21 @@ using namespace std;
 class Solution {
 public:
     vector<int> numsSameConsecDiff(int N, int K) {
-
+        vector<int> res = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        for (int i = 1; i < N; ++i) {
+            vector<int> tmp;
+            for (auto num: res) {
+                auto last = num % 10;
+                if (num > 0 && last + K < 10) {
+                    tmp.push_back(10 * num + last + K);
+                }
+                if (num > 0 && K > 0 && last - K >= 0) {
+                    tmp.push_back(10 * num + last - K);
+                }
+            }
+            res = tmp;
+        }
+        return res;
     }
 };
 
