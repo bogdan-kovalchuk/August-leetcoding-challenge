c807db81a873b1bd52af7c8b5ec4006f597b73ff
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-08-21 12:00:45 +0300

    day 21 commit

--- day_21.cpp
+++ day_21.cpp
@@ -6,7 +6,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int> &A) {
-
+        vector<int> out;
+        for (int & i : A) {
+            if (i % 2) {
+                out.push_back(i);
+            } else {
+                out.insert(out.begin(), i);
+            }
+        }
+        return out;
     }
 };
 
@@ -14,7 +22,7 @@ int main() {
     vector<int> A = {3, 1, 2, 4};
     Solution solution;
     auto res = solution.sortArrayByParity(A);
-    for(auto n : res) {
+    for (auto n : res) {
         std::cout << n << " ";
     }
 }
