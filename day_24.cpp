54f43beab027c016d37ce4f399a257d97c0c0f47
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-08-24 20:27:33 +0300

    day 24 commit

---
+++ day_24.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode *root) {
+
+    }
+};
+
+int main() {
+    auto head = new TreeNode(3);
+    head->left = new TreeNode(9);
+    head->right = new TreeNode(20);
+    head->right->left = new TreeNode(15);
+    head->right->right = new TreeNode(7);
+
+    Solution solution;
+    std::cout << solution.sumOfLeftLeaves(head);
+
+    return 0;
+}
