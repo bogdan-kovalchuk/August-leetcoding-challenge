245380085a5de4716080a1578dcdcd2d6d56fd41
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2020-09-28 10:00:00 +0300

    add cycle-sort duplicate finder alternative

--- day_06.cpp
+++ day_06.cpp
@@ -28,3 +28,24 @@ int main() {
     }
     return 0;
 }
+// --- Alternative: swap-based cycle placement ---
+// Time O(n), Space O(1) - each element swapped at most twice into place
+// Edge cases: duplicates end up at wrong indices after all swaps complete
+// Compare: original negates values to mark seen (mutates signs);
+// cycle sort physically moves elements to correct index - same O(n) time
+// but different mutation pattern; works even if sign bit is needed
+class SolutionCycle {
+public:
+    vector<int> findDuplicates(vector<int> &nums) {
+        vector<int> out;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] != nums[nums[i] - 1])
+                swap(nums[i], nums[nums[i] - 1]);
+        }
+        for (int i = 0; i < n; ++i)
+            if (nums[i] != i + 1)
+                out.push_back(nums[i]);
+        return out;
+    }
+};
