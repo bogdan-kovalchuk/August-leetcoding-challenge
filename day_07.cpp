1fa529cbde3f90715d85cd87039bb71224ff67d6
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2020-10-02 10:00:00 +0300

    add BFS vertical traversal alternative

--- day_07.cpp
+++ day_07.cpp
@@ -67,3 +67,42 @@ int main() {
     return 0;
 }
 
+
+// --- Alternative: BFS with column tracking ---
+// Time O(n log n) for final sort, Space O(n)
+// Edge cases: level-order guarantees row ordering; values at same (col,row) sorted
+// Compare: original uses nested map (auto-sorted but high constant factor);
+// BFS collects then sorts - same result with potentially better cache behavior
+#include <climits>
+#include <tuple>
+#include <queue>
+class SolutionBFS {
+    struct Cell { int col, row, val; };
+public:
+    vector<vector<int>> verticalTraversal(TreeNode *root) {
+        if (!root) return {};
+        vector<Cell> cells;
+        queue<pair<TreeNode*, pair<int,int>>> q;
+        q.push({root, {0, 0}});
+        while (!q.empty()) {
+            auto front = q.front(); q.pop();
+            TreeNode* node = front.first;
+            int x = front.second.first, y = front.second.second;
+            cells.push_back({x, y, node->val});
+            if (node->left)  q.push({node->left,  {x - 1, y + 1}});
+            if (node->right) q.push({node->right, {x + 1, y + 1}});
+        }
+        sort(cells.begin(), cells.end(), [](const Cell& a, const Cell& b) {
+            if (a.col != b.col) return a.col < b.col;
+            if (a.row != b.row) return a.row < b.row;
+            return a.val < b.val;
+        });
+        vector<vector<int>> res;
+        int prevCol = INT_MIN;
+        for (auto& c : cells) {
+            if (c.col != prevCol) { res.emplace_back(); prevCol = c.col; }
+            res.back().push_back(c.val);
+        }
+        return res;
+    }
+};
