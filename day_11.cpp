8a958adea0b083eb521e786df92f8023fbce8db6
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2021-03-28 10:00:00 +0300

    add counting sort h-index alternative

--- day_11.cpp
+++ day_11.cpp
@@ -21,9 +21,34 @@ public:
     }
 };
 
+class SolutionCounting {
+public:
+    int hIndex(vector<int> &citations) {
+        int n = citations.size();
+        vector<int> buckets(n + 1, 0);
+        for (int c : citations) {
+            buckets[std::min(c, n)]++;
+        }
+        int cumulative = 0;
+        for (int i = n; i >= 0; --i) {
+            cumulative += buckets[i];
+            if (cumulative >= i) return i;
+        }
+        return 0;
+    }
+};
+
 int main() {
-    vector<int> citations = {11, 15};
-    Solution solution;
-    std::cout << solution.hIndex(citations) << std::endl;
+    vector<vector<int>> tests = {{11, 15}, {3, 0, 6, 1, 5}, {1, 3, 1}, {0}, {100}};
+    Solution orig;
+    SolutionCounting counting;
+    const char *labels[] = {"test1", "test2", "test3", "test4_single_zero", "test5_single_high"};
+    for (int t = 0; t < (int)tests.size(); ++t) {
+        vector<int> c1 = tests[t], c2 = tests[t];
+        int r1 = orig.hIndex(c1);
+        int r2 = counting.hIndex(c2);
+        std::cout << labels[t] << ": orig=" << r1 << " counting=" << r2
+                  << (r1 == r2 ? " [match]" : " [MISMATCH]") << std::endl;
+    }
     return 0;
 }
