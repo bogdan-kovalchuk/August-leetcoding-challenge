4ea5c9219b232d379d335ecff058706c084f6614
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2021-04-19 10:30:00 +0300

    add recursive backtracking combination iterator

--- day_13.cpp
+++ day_13.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <vector>
 
 using std::string;
 using std::set;
+using std::vector;
 
 class CombinationIterator {
 public:
@@ -35,15 +37,57 @@ private:
     set <string> :: iterator it;
 };
 
-int main() {
-    auto iterator = new CombinationIterator("bvwz", 2); // creates the iterator.
+class CombinationIteratorBacktrack {
+public:
+    CombinationIteratorBacktrack(string characters, int combinationLength) {
+        string current;
+        backtrack(characters, combinationLength, 0, current);
+        idx = 0;
+    }
 
-    std::cout << iterator->next() << std::endl; // returns "ab"
-    std::cout << iterator->hasNext() << std::endl; // returns true
-    std::cout << iterator->next() << std::endl; // returns "ac"
-    std::cout << iterator->hasNext() << std::endl; // returns true
-    std::cout << iterator->next() << std::endl; // returns "bc"
-    std::cout << iterator->hasNext() << std::endl; // returns false
+    string next() {
+        return combinations[idx++];
+    }
+
+    bool hasNext() {
+        return idx < (int)combinations.size();
+    }
+
+private:
+    vector<string> combinations;
+    int idx;
+
+    void backtrack(const string &chars, int len, int start, string &current) {
+        if ((int)current.size() == len) {
+            combinations.push_back(current);
+            return;
+        }
+        for (int i = start; i < (int)chars.size(); ++i) {
+            current.push_back(chars[i]);
+            backtrack(chars, len, i + 1, current);
+            current.pop_back();
+        }
+    }
+};
+
+int main() {
+    auto test_pair = [](const string &chars, int len, const char *label) {
+        CombinationIterator bitmask(chars, len);
+        CombinationIteratorBacktrack bt(chars, len);
+        std::cout << label << ":";
+        while (bitmask.hasNext() && bt.hasNext()) {
+            string s1 = bitmask.next();
+            string s2 = bt.next();
+            std::cout << " " << s1;
+            if (s1 != s2) std::cout << "[MISMATCH:" << s2 << "]";
+        }
+        std::cout << std::endl;
+    };
+
+    test_pair("bvwz", 2, "4C2");
+    test_pair("abc", 2, "3C2");
+    test_pair("abcd", 1, "4C1");
+    test_pair("abcde", 3, "5C3");
 
     return 0;
 }
