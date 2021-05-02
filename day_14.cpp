21237049258f64eccdf28dd4255ec09fc4251d3a
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2021-05-02 12:00:00 +0300

    add array-based palindrome length alternative

--- day_14.cpp
+++ day_14.cpp
@@ -7,6 +7,8 @@ using std::unordered_map;
 
 class Solution {
 public:
+    // Hash map counting: O(n) time, O(sigma) space where sigma = distinct chars
+    // unordered_map has higher constant factor than fixed array for ASCII input
     int longestPalindrome(string s) {
         if (s.empty()) return 0;
         unordered_map<char, int> m;
@@ -27,10 +29,34 @@ public:
     }
 };
 
+class SolutionArray {
+public:
+    // Fixed array counting: O(n) time, O(1) space (128-int array for ASCII)
+    // Avoids hash overhead; direct index by char value
+    int longestPalindrome(string s) {
+        int counts[128] = {};
+        for (char c : s) counts[c]++;
+        int len = 0;
+        bool has_odd = false;
+        for (int i = 0; i < 128; ++i) {
+            len += counts[i] / 2 * 2;
+            if (counts[i] % 2) has_odd = true;
+        }
+        return len + (has_odd ? 1 : 0);
+    }
+};
+
 int main() {
-    string s = "abccccdd";
-    Solution solution;
-    std::cout << solution.longestPalindrome(s) << std::endl;
+    Solution orig;
+    SolutionArray arr;
+    string tests[] = {"abccccdd", "a", "bb", "abcdef", "aaaaaacccccccccc"};
+    const char *labels[] = {"mixed", "single", "pair", "all_unique", "heavy_repeat"};
+    for (int t = 0; t < 5; ++t) {
+        int r1 = orig.longestPalindrome(tests[t]);
+        int r2 = arr.longestPalindrome(tests[t]);
+        std::cout << labels[t] << ": orig=" << r1 << " array=" << r2
+                  << (r1 == r2 ? " [match]" : " [MISMATCH]") << std::endl;
+    }
     return 0;
 }
 
